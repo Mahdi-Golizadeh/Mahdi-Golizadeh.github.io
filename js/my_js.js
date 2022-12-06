0d8eead3ae39234b3a18fbe079eb5e4361002173
Mahdi-Golizadeh <mastergolizadeh@gmail.com>
2022-12-06 17:34:14 +0330

    git commit -a

--- js/my_js.js
+++ js/my_js.js
@@ -1,1 +1,4 @@
-alert("welcome to my page!")
+alert("Hi!");
+let name1 = prompt("enter your name please", "mahdi");
+alert("welcome " + name1);
+document.getElementById("hih").innerHTML = "welocme " + name1
